0d2c52a36e1569900f71e943097a2717ef82d57d
molodoypatsan <89076616+molodoypatsan@users.noreply.github.com>
2021-08-19 16:23:55 +0700

    Скрипт для подсветки этажей и изменения счетчика

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    var currentFloor = 2; // переменная, где хранится текущий этаж
+    var floorPath = $(".home-image path"); // каждый отдельный этаж в SVG
+    var counterUp = $(".counter-up"); /* кнопка увеличения этажа */
+    var counterDown = $(".counter-down"); /* кнопка уменьшения этажа */
+    
+    // функция при наведении мышью на этаж
+    floorPath.on("mouseover", function () {
+        floorPath.removeClass("current-floor"); // удаляем активный класс у этажей
+        currentFloor = $(this).attr("data-floor"); // получаем значение текущего этажа
+        $(".counter").text(currentFloor); // записываем значение этажа в счетчик справа
+    });
+    // отслеживаем клик по кнопке вверх
+    counterUp.on("click", function () {
+        // проверяем значение этажа, оно не должно быть больше 18
+        if (currentFloor < 18) {
+            currentFloor++; // прибавляем один этаж
+            usCurrentFloor = currentFloor.toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false}); // форматируем переменную с этажом, чтобы было 01 а не 1
+            $(".counter").text(usCurrentFloor); // записываем значение этажа в счетчик справа
+            floorPath.removeClass("current-floor"); // удаляем активный класс у этажей
+            $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor"); // подсвечиваем текущий этаж
+        }
+    });
+        // отслеживаем клик по кнопке вниз
+    counterDown.on("click", function () {
+        // проверяем значение этажа, оно должно быть больше 2
+        if (currentFloor > 2) {
+            currentFloor--; // отнимаем один этаж
+            usCurrentFloor = currentFloor.toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false}); // форматируем переменную с этажом, чтобы было 01 а не 1
+            $(".counter").text(usCurrentFloor); // записываем значение этажа в счетчик справа
+            floorPath.removeClass("current-floor"); // удаляем активный класс у этажей
+            $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor"); // подсвечиваем текущий этаж
+        }
+    });
+});
